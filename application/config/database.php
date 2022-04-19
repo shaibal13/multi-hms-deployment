77845ce4a5110c692d87bd8e6e32c318d3e3ec73
shaibal13 <sahashaibal22@gmail.com>
2022-04-19 10:03:05 +0600

    database

--- application/config/database.php
+++ application/config/database.php
@@ -70,16 +70,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
+//hospital.mysql.database.azure.com
 $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'hospital.mysql.database.azure.com', 
+	'hostname' => 'localhost', 
 	'username' => 'hospital_codearistos',
 	'password' => 'Ohmygod11$$',
 	'database' => 'hospital_database', 
-	'dbdriver' => 'mysql',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
